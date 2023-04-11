a934542da0b30521630f999d03e8a9fa7a04df56
Ananth Prayaga <ananthp@uchicago.edu>
2023-04-11 14:44:51 +0000

    Completed all of Zodiacs and Unlucky

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -4,10 +4,21 @@ class NumbersController < ApplicationController
 
     5.times do
       giraffe = rand(1...100)
-      
+
       @zebra.push(giraffe)
     end
 
-    render({ :template => "lottery_stuff/woohoo.html.erb"})
-  end 
+    render({ :template => "lottery_stuff/woohoo.html.erb" })
+  end
+
+  def losers
+    @lost_numbers = Array.new
+
+    5.times do
+      number = rand(1...100)
+      @lost_numbers.push(number)
+    end
+
+    render({ :template => "lottery_stuff/boohoo.html.erb" })
+  end
 end
